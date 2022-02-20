eb65bbdd836e883750a8eeec2c2d9187868fcc6f
Adnan Muzavor <www.adnan9168389872@gmail.com>
2022-02-21 00:32:08 +0530

    Created signin and signup

---
+++ frontend/src/Componenets/Authentication/login.js
@@ -0,0 +1,114 @@
+import {
+  FormControl,
+  FormLabel,
+  VStack,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Button,
+} from "@chakra-ui/react";
+import React, { useImperativeHandle, useState } from "react";
+
+const Login = () => {
+    //States to take care of inputs
+  const [name, setname] = useState("");
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+
+  const [show, setshow] = useState(false);
+  //Function to deal with profile photo upload
+  const postdetails=(file)=>{
+
+  }
+  //Function to submit data
+  const SubmitHandler=()=>{
+
+  }
+  return (
+    <VStack spacing={"5px"}>
+        {/* Any form component can be wrapped inside fromcontrol component */}
+      <FormControl id="firstname" isRequired>
+          {/* Form control has two things label and input */}
+        <FormLabel>Name</FormLabel>
+        <Input
+          placeholder="Enter your name"
+          onChange={(e) => setname(e.target.value)}
+        ></Input>
+      </FormControl>
+      <FormControl id="email" isRequired>
+        <FormLabel>Email</FormLabel>
+        <Input
+          type="email"
+          placeholder="Enter your email"
+          onChange={(e) => setemail(e.target.value)}
+        ></Input>
+      </FormControl>
+      <FormControl id="password" isRequired>
+        <FormLabel>password</FormLabel>
+        {/* Input group is used when there are to be more then one things for input */}
+        {/* It basically has left and right elements */}
+        <InputGroup>
+          <Input
+            type={show ? "text" : "password"}
+            placeholder="Enter your password"
+            onChange={(e) => setpassword(e.target.value)}
+          ></Input>
+          <InputRightElement width={"4.5rem"}>
+            <Button
+              h="2rem"
+              size={"sm"}
+              onClick={() => {
+                show === true ? setshow(false) : setshow(true);
+              }}
+            >
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+
+      {/* <FormControl id="password" isRequired>
+        <FormLabel>confirm password</FormLabel>
+      
+        <InputGroup>
+          <Input
+            type={show ? "text" : "password"}
+            placeholder="Enter your password"
+            onChange={(e) => setconfirmpassword(e.target.value)}
+          ></Input>
+          <InputRightElement width={"4.5rem"}>
+            <Button
+              h="2rem"
+              size={"sm"}
+              onClick={() => {
+                show === true ? setshow(false) : setshow(true);
+              }}
+            >
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl> */}
+
+
+      {/* <FormControl id="Pic" isRequired>
+        <FormLabel>Upload your picture</FormLabel>
+        <Input
+          type="file"
+          p={1.5}
+          accept="image/*"
+          placeholder="Upload your pic"
+          onChange={(e) =>postdetails(e.target.files[0])}
+        ></Input>
+      </FormControl> */}
+      <Button
+      colorScheme={'blue'}
+      width="100%"
+      style={{marginTop:15}}
+      onClick={()=>SubmitHandler()}
+      >Login</Button>
+    </VStack>
+  );
+};
+
+export default Login;
--- frontend/src/Screens/Home.js
+++ frontend/src/Screens/Home.js
@@ -1,18 +1,49 @@
+import { Container, Box, Text } from "@chakra-ui/react";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
 import React from "react";
+import Login from "../Componenets/Authentication/login";
+import Signup from "../Componenets/Authentication/signup";
 import Chatlist from "./Chatlist";
 import Chatpage from "./ChatPage";
 
 const Homepage = () => {
   return (
     <>
-      <div className="container">
-        <div className="row">
-          {/* chat list component */}
-          <Chatlist />
-          {/*Chat component */}
-          <Chatpage />
-        </div>
-      </div>
+      <Container maxW="xl" centerContent>
+        <Box
+          bg={"#ffff"}
+          justifyContent="center"
+          padding={3}
+          d="flex"
+          w="100%"
+          m="40px 0 15px 0"
+          borderRadius={"lg"}
+          borderWidth="1px"
+        >
+          <Text fontSize="3xl" fontFamily="word-sans">
+            Talk-to-tive
+          </Text>
+        </Box>
+          <div className="container">
+          <Tabs variant={'soft-rounded'}>
+            <TabList mb='1em'>
+              <Tab width="50%">Login</Tab>
+              <Tab width="50%">Sign-up</Tab>
+            
+            </TabList>
+
+            <TabPanels>
+              <TabPanel>
+                <Login/>
+              </TabPanel>
+              <TabPanel>
+                <Signup/>
+              </TabPanel>
+             
+            </TabPanels>
+          </Tabs>
+          </div>
+      </Container>
     </>
   );
 };
